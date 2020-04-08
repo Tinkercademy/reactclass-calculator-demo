16f356c6d70ce4fa461b3f4e01b319ad9e06f8c0
yuhaochua <yuhaochua99@gmail.com>
2020-04-08 13:39:11 +0800

    Rendering elements

--- src/App.js
+++ src/App.js
@@ -5,8 +5,43 @@ import './App.css';
 class App extends Component {
   render(){
     return (
-      <div className="App">
-          
+      <div className="App pt-5">
+          <h1 className="d-flex justify-content-center">Calculator</h1>
+
+          <div className="d-flex justify-content-center mt-5">
+            <textarea className="result" readOnly></textarea>
+          </div>
+
+          <div className="d-flex justify-content-center mt-5">
+            <input type="button" className="btn btn-danger mx-2" value ="C"/>
+            <input type="button" className="btn btn-primary mx-2 invisible"/>
+            <input type="button" className="btn btn-warning mx-2" value ="DEL"/>
+            <input type="button" className="btn btn-success mx-2" value ="/"/>
+          </div>         
+          <div className="d-flex justify-content-center mt-2">
+            <input type="button" className="btn btn-primary mx-2" value ="7"/>
+            <input type="button" className="btn btn-primary mx-2" value ="8"/>
+            <input type="button" className="btn btn-primary mx-2" value ="9"/>
+            <input type="button" className="btn btn-success mx-2" value ="*"/>
+          </div>
+          <div className="d-flex justify-content-center mt-2">
+            <input type="button" className="btn btn-primary mx-2" value ="4"/>
+            <input type="button" className="btn btn-primary mx-2" value ="5"/>
+            <input type="button" className="btn btn-primary mx-2" value ="6"/>
+            <input type="button" className="btn btn-success mx-2" value ="-"/>
+          </div>
+          <div className="d-flex justify-content-center mt-2">
+            <input type="button" className="btn btn-primary mx-2" value ="1"/>
+            <input type="button" className="btn btn-primary mx-2" value ="2"/>
+            <input type="button" className="btn btn-primary mx-2" value ="3"/>
+            <input type="button" className="btn btn-success mx-2" value ="+"/>
+          </div>
+          <div className="d-flex justify-content-center mt-2">
+            <input type="button" className="btn btn-primary mx-2" value ="00"/>
+            <input type="button" className="btn btn-primary mx-2" value ="0"/>
+            <input type="button" className="btn btn-primary mx-2" value ="."/>
+            <input type="button" className="btn btn-success mx-2" value ="="/>
+          </div>
       </div>
     );
   }
